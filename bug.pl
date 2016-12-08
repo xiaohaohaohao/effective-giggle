00324fee4e3a22eb6720baa7f21f834e977b2e59
changhao <1023630505@qq.com>
2016-12-08 10:09:22 +0800

    BUG

---
+++ bug.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+use strict;
+my $dna='CGACGTCTTCTAAGGCGA';
+my @dna;
+my $receivingcommittment;
+my $previousbase='';
+my $subsequence='';
+if(@ARGV){
+$subsequence=$ARGV[0];
+}
+else{
+$subsequence='TA';
+}
+my $base1=substr($subsequence,0,1);
+my $base2=substr($subsequence,1,1);
+@dna=split('',$dna);
+foreach(@dna){
+if($receivingcommittment){
+print;
+next;
+}elsif($previousbase eq $base1){
+if(/$base2/){
+print $base1,$base2;
+$receivingcommittment=1;
+}
+}
+$previousbase=$_;
+}
+print"\n";
+exit;
